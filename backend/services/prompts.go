9ad478ca77a3e867f88d8332b0b7b6b408ee929e
1234arushi <24arushisharma@gmail.com>
2026-06-29 22:31:25 +0530

    change in prompt after testing

--- backend/services/prompts.go
+++ backend/services/prompts.go
@@ -9,7 +9,6 @@ Return STRICT JSON.
 {
   "required_skills": [],
   "nice_to_have_skills": [],
-  "education": [],
   "experience": ""
 }
 
@@ -18,8 +17,12 @@ Rules:
 - Ignore company culture, benefits, diversity statements, hiring process and marketing.
 - Include only explicitly mentioned skills.
 - Put mandatory skills in required_skills.
-- Put preferred skills in nice_to_have_skills.
-- Do not return explanations.
+- Put preferred/additional skills which are less imp according to job poster in nice_to_have_skills,otherwise leave it empty.
+- Do not return explanations,simply keywords.
 - Return valid JSON only.
+- Do not wrap the response in json tags
+- Do not use markdown.
+- Do not add explanations.
+- Do not add any text before or after the JSON.
 
 `
